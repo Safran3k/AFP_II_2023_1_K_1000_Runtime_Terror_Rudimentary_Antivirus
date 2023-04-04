06304478b7375bb3db3d2ecabb3783b2f4663bf8
turmxhun <103107003+turmxhun@users.noreply.github.com>
2023-04-04 10:43:57 +0200

    Regisztráció és bejelentkezéshez a kód megírva

--- Rudimentary_Antivirus/Rudimentary_Antivirus/LoginWindow.xaml.cs
+++ Rudimentary_Antivirus/Rudimentary_Antivirus/LoginWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,17 +29,84 @@ namespace Rudimentary_Antivirus
 
         private void btn_Login_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["recipeConnString"].ConnectionString);
 
+            string data = "SELECT * FROM Users WHERE username = '" + tbUserName.Text + "'";
+
+            string command = "SELECT COUNT(1) FROM Users WHERE Username = @username AND Password = @password";
+
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                string result = string.Empty;
+                bool admin = false;
+                string username = string.Empty;
+
+                SqlCommand sqlCom1 = new SqlCommand(data, connection);
+                using (var reader = sqlCom1.ExecuteReader(CommandBehavior.SingleRow))
+                {
+                    if (reader.HasRows)
+                    {
+                        if (reader.Read())
+                        {
+                            result = reader["Password"].ToString().Trim();
+                            admin = Convert.ToBoolean(reader["IsAdmin"]);
+                            username = reader["Username"].ToString();
+                        }
+
+                    }
+                }
+
+                if (Encryption(tbPassword.Password.Trim()) == result)
+                {
+                    SqlCommand sqlCom2 = new SqlCommand(command, connection);
+                    sqlCom2.CommandType = CommandType.Text;
+                    sqlCom2.Parameters.AddWithValue("@username", tbUserName.Text.Trim());
+                    sqlCom2.Parameters.AddWithValue("@password", Encryption(tbPassword.Password.Trim()));
+                    int id = Convert.ToInt32(sqlCom2.ExecuteScalar());
+
+                    if (id > 0)
+                    {
+                        MainWindow newWindow = new MainWindow(admin, username);
+                        newWindow.Show();
+                        connection.Close();
+                        Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Wrong username or password", "Login failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can not connect to the database", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void btn_Registration_Click(object sender, RoutedEventArgs e)
         {
-
+            RegistrationWindow newWindow = new RegistrationWindow();
+            newWindow.Show();
+            Close();
         }
 
         private void btn_Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            if (MessageBox.Show("Are you sure?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Environment.Exit(0);
+            }
         }
+        public static string Encryption(string password)
+        {
+            var textBytes = Encoding.UTF8.GetBytes(password);
+            return Convert.ToBase64String(textBytes);
+        }
+
     }
 }
--- Rudimentary_Antivirus/Rudimentary_Antivirus/RegistrationWindow.xaml.cs
+++ Rudimentary_Antivirus/Rudimentary_Antivirus/RegistrationWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -26,7 +28,75 @@ namespace Rudimentary_Antivirus
 
         private void btn_Registration_Click(object sender, RoutedEventArgs e)
         {
+            if (tbUserName.Text == "" || tbPassword.Password == "")
+            {
+                MessageBox.Show("Username and password can't be empty", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (tbPassword.Password != tbPasswordAgain.Password)
+            {
+                MessageBox.Show("Passwords aren't matching", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
 
+                SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["recipeConnString"].ConnectionString);
+
+                try
+                {
+                    if (connection.State == System.Data.ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+
+                    string command1 = "SELECT id FROM users WHERE username = @username";
+
+                    SqlCommand sqlCom1 = new SqlCommand(command1, connection);
+                    sqlCom1.CommandType = System.Data.CommandType.Text;
+                    sqlCom1.Parameters.AddWithValue("@username", tbUserName.Text);
+                    int id = Convert.ToInt32(sqlCom1.ExecuteScalar());
+
+                    if (id > 0)
+                    {
+                        MessageBox.Show("This username already exists", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                    }
+                    else
+                    {
+                        string command2 = "INSERT INTO users(username, password, isAdmin) VALUES('" + tbUsername.Text + "', '" + Enryption(tbPassword.Password.ToString()) + "', '" + 0 + "')";
+
+                        SqlCommand sqlCom2 = new SqlCommand(command2, connection);
+                        sqlCom2.CommandType = System.Data.CommandType.Text;
+                        sqlCom2.ExecuteNonQuery();
+
+                        MessageBox.Show("Successful registration", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                        LoginWindow newWindow = new LoginWindow();
+                        newWindow.Show();
+                        Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private void btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow newWindow = new LoginWindow();
+            newWindow.Show();
+            Close();
+        }
+
+        public static string Enryption(string password)
+        {
+            var textBytes = Encoding.UTF8.GetBytes(password);
+            return Convert.ToBase64String(textBytes);
         }
     }
 }
