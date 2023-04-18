c1f8fb79556bd788aa50762f537ac8983f590c3a
safran3k <futo.ricsi@gmail.com>
2023-04-18 10:52:33 +0200

    Create getFileHashCode function

--- API/hashCodes.php
+++ API/hashCodes.php
@@ -15,3 +15,10 @@ switch ($request) {
         header('HTTP/1.1 405 Method Not Allowed');
         break;
 }
+
+function getFileHashCode($fileHashCode) {
+	global $connection;
+	
+	$result = count($connection -> query("SELECT * FROM antivirushashcodes WHERE hashCode = '$fileHashCode' ") -> fetch_all());
+	return $result > 0;
+}
