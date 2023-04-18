e1f87ef17d59bac4605764aa71abc1d118765a0b
safran3k <futo.ricsi@gmail.com>
2023-04-18 10:55:36 +0200

    Create flagFileHashCodeAndStore function
    Flag a file for suspicious behavior, and store it's hash code

--- API/hashCodes.php
+++ API/hashCodes.php
@@ -22,3 +22,25 @@ function getFileHashCode($fileHashCode) {
 	$result = count($connection -> query("SELECT * FROM antivirushashcodes WHERE hashCode = '$fileHashCode' ") -> fetch_all());
 	return $result > 0;
 }
+
+function flagFileHashCodeAndStore($hashCode) {
+  global $connection;
+  $count = count($connection -> query("SELECT * FROM antivirushashcodes WHERE hashCode = '$hashCode';") -> fetch_all(MYSQLI_ASSOC));
+  
+  if ($count == 0) {
+    $result = $connection -> query("INSERT INTO antivirushashcodes (hashCode, dateAdded) VALUES ('$hashCode', 'date()');");
+    echo json_encode(
+      array(
+        'error' => 0,
+        'message' => 'Actual file flagged, and stored in database!'
+      )
+    );
+  }
+  else {
+    echo json_encode(array(
+        'error' => 1,
+        'message' => 'Actual file hash code is already in database!'
+      )
+    );
+  }
+}
